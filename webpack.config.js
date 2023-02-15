d4e28c5ebf9f77c54e1c028ba575e369cdce4c8c
pranavwecallmedia <pranav@wecallmedia.com>
2023-02-15 13:23:51 +0530

    changed public url

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:8082/",
+    publicPath: "https://candid-pithivier-dd1f7e.netlify.app/",
   },
 
   resolve: {
