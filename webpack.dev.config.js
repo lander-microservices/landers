d0e1460f5079d037ae7d28e407582dce68f1b476
Pranav Kumar <pranav@wecallmedia.com>
2023-04-07 04:02:37 +0530

    Fixed Bugs

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -77,7 +77,6 @@ module.exports = {
         components: "components@http://localhost:8084/remoteEntry.js",
       },
       exposes: {
-        "./Lander1": "./src/pages/lander1",
         "./V1Lander": "./src/pages/v1/lander",
         "./V1Footer": "./src/pages/v1/footer",
         "./V1Header": "./src/pages/v1/header"
