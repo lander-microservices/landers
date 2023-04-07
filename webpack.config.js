22f72965e586ed4b37d44e397d5ffc0f023e8288
Pranav Kumar <pranav@wecallmedia.com>
2023-04-08 04:18:18 +0530

    Added Gzip Compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 const fs = require("fs");
 try {
@@ -93,6 +94,7 @@ module.exports = {
         },
       },
     }),
+    new CompressionPlugin(),
     new HtmlWebPackPlugin({
       template: "./src/index.html",
     }),
