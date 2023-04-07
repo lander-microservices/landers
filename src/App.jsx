75b49f8bf25761ca8007e08c5b601244902417a6
Pranav Kumar <pranav@wecallmedia.com>
2023-04-07 19:42:27 +0530

    Removed Unused Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import Lander1 from "./pages/lander1"
 import "./index.css";
 
 const App = () => (
   <>
-    <Lander1 init={()=> {}} number="(800)888888" callClickCb={()=>{}} />
   </>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
