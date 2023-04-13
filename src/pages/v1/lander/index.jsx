630a3fd411aaca5698433e7e256e3e3de3f9220a
Pranav Kumar <pranav@wecallmedia.com>
2023-04-13 22:27:50 +0530

    Fixed Bugs

--- src/pages/v1/lander/index.jsx
+++ src/pages/v1/lander/index.jsx
@@ -1,6 +1,5 @@
 import Quiz from "./Quiz";
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./index.scss";
 
 function LanderParagraphSection({
